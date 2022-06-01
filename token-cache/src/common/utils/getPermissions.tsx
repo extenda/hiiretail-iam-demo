220ed685b7fc9868f39c6ed2140e190145c700c9
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2022-06-01 14:28:41 +0300

    comments

--- token-cache/src/common/utils/getPermissions.tsx
+++ token-cache/src/common/utils/getPermissions.tsx
@@ -1,8 +1,3 @@
 export const getPermissions = async (): Promise<string[]> => {
-  // const token = await firebase.app().auth().currentUser?.getIdToken();
-  // const tenantId = localStorage.getItem('tenantId') as string;
-
-
-//  Use IAM API (https://developer.hiiretail.com/api/iam-api#operation/getOwnPermissions) to fetch user's permissions here
   return [];
 };
