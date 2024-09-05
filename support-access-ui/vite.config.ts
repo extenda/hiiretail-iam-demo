2903d3cb837dcdd325716041d18c6cf6a3a8e59b
Gleb Bashkatov <glebbash@gmail.com>
2024-09-05 14:32:31 +0000

    chore: try build for githack

--- support-access-ui/vite.config.ts
+++ support-access-ui/vite.config.ts
@@ -3,5 +3,6 @@ import solid from "vite-plugin-solid";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
+  base: "https://raw.githack.com/extenda/hiiretail-iam-demo/feat/HII-10108-poc-support-ui/support-access-ui/dist/",
   plugins: [solid(), nodePolyfills({ include: ["crypto", "util", "stream"] })],
 });
